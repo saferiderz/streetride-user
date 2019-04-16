84b77ae3818a64a81215f7266bf8409fe19560de
Chris <chris.m.hein@gmail.com>
2019-04-15 20:06:35 -0400

    added comments

--- streetride-user/models/users.js
+++ streetride-user/models/users.js
@@ -7,7 +7,8 @@ function Users (sequelize, DataTypes) {
             // hard coding in 'anonymousUser' for folks who want to continue on as a guest without creating an account
             defaultValue: 'anonymousUser',
 			validate: {
-				len: [1]
+                //username must be at least 5 character long
+				len: [5]
 			}
 		},
 		password: {
@@ -16,6 +17,7 @@ function Users (sequelize, DataTypes) {
             // hard coding anonymousUserPassword for folks who want to coninue on as a guest without creating an account
             defaultValue: 'anonymousUserPassword',
 			validate: {
+                //password must be at least 8 characters long
 				len: [8]
 			}
 		}
