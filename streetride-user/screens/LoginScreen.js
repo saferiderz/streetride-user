a044fe010d4822118490d583478884ee441129d3
Kevin Patel <kpatel1209@gmail.com>
2019-04-21 17:07:29 -0400

    trying to implement scroll view on create account page

--- streetride-user/screens/LoginScreen.js
+++ streetride-user/screens/LoginScreen.js
@@ -34,7 +34,9 @@ import ViewIssues from '../screens/ViewIssues'
               secureTextEntry={true}
               placeholderTextColor="#000080"
             />
-            <TouchableOpacity style={styles.buttonNavy} >
+            <TouchableOpacity style={styles.buttonNavy} onPress={() => {
+                this.props.navigation.navigate("View")
+              }}>
               <Text style={styles.buttonTextSubmit}>Login</Text>
             </TouchableOpacity>
             <TouchableOpacity
@@ -61,48 +63,50 @@ import ViewIssues from '../screens/ViewIssues'
 class CreateAccount extends React.Component {
   render() {
     return (
-      
+      <ScrollView>
         <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
           <Text style={styles.headerText}>Create an Account</Text>
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="First Name"
-            placeholderTextColor="#000080"
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Last Name"
-            placeholderTextColor="#000080"
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Username"
-            placeholderTextColor="#000080"
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Password"
-            secureTextEntry={true} placeholderTextColor="#000080"
-          />
-          <TouchableOpacity style={styles.buttonNavy} onPress={() => {
-                this.props.navigation.navigate("View")
-              }}>
-            <Text style={styles.buttonTextSubmit}>Submit</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.button}
-            onPress={() => {
-              this.props.navigation.navigate("Login")
-            }}
-          >
-            <Text style={styles.buttonText}>Go Back to Login Screen</Text>
-          </TouchableOpacity>
-
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputBox}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              placeholder="First Name"
+              placeholderTextColor="#000080"
+            />
+            <TextInput
+              style={styles.inputBox}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              placeholder="Last Name"
+              placeholderTextColor="#000080"
+            />
+            <TextInput
+              style={styles.inputBox}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              placeholder="Username"
+              placeholderTextColor="#000080"
+            />
+            <TextInput
+              style={styles.inputBox}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              placeholder="Password"
+              secureTextEntry={true} placeholderTextColor="#000080"
+            />
+            <TouchableOpacity style={styles.buttonNavy} onPress={() => {
+                  this.props.navigation.navigate("View")
+                }}>
+              <Text style={styles.buttonTextSubmit}>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={styles.button}
+              onPress={() => {
+                this.props.navigation.navigate("Login")
+              }}
+            >
+              <Text style={styles.buttonText}>Go Back to Login Screen</Text>
+            </TouchableOpacity>
+          </View>
         </KeyboardAvoidingView>
+      </ScrollView>
     )
   }
 }
@@ -172,8 +176,8 @@ const styles = StyleSheet.create({
     marginTop: 5
   },
   headerText: {
-    marginBottom: 10,
-    marginTop: 30,
+    marginBottom: 50,
+    marginTop: 10,
     color: 'rgba(0,0,0,0.9)',
     fontSize: 40,
     textAlign: 'center',
