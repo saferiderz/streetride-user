4cb1070118d724c6fdb76eff29813c199cbd742b
Chris <chris.m.hein@gmail.com>
2019-04-22 19:57:46 -0400

    initial commit

---
+++ streetride-user/components/ReportOrView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import {KeyboardAvoidingView} from 'react-native';
+import Logo from "./Logo";
+
+ export default class LoginForm extends React.Component {
+     
+ }
