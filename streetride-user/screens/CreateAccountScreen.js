a3802f813af0f14f88b5a980cb2a3d3d3786fc21
Kevin Patel <kpatel1209@gmail.com>
2019-04-28 18:15:10 -0400

    added firstname and lastname to the state

--- streetride-user/screens/CreateAccountScreen.js
+++ streetride-user/screens/CreateAccountScreen.js
@@ -12,6 +12,8 @@ import { KeyboardAvoidingView } from "react-native";
 
 export default class CreateAccountScreen extends Component {
   state = {
+    firstname = "",
+    lastname = "",
     username: "",
     password: ""
   };
