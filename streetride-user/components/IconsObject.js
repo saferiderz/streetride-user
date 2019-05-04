4f3551c1ccb94d19617a899c1960157db2c9b9dd
Chris <chris.m.hein@gmail.com>
2019-05-04 16:39:03 -0400

    updated descriptions for all

--- streetride-user/components/IconsObject.js
+++ streetride-user/components/IconsObject.js
@@ -2,42 +2,42 @@ const Icons = {
     CarInBikeLane: {
         name: 'Car In Bike Lane',
         uri: require("../assets/images/traffic.png"),
-        description: 'Car parked in bike lane \nDelivery truck in bike lane'
+        description: 'Car parked in bike lane \n\nDelivery truck in bike lane'
     },
     CloseCall: {
         name: 'Close Call',
         uri: require("../assets/images/close.png"),
-        description:
-            `A vehicle passing within 3 feet of a bicycle or scooter on the road
-
-        A vehicle nearly hitting a pedestrian in a crosswalk`
+        description: 'A vehicle passing within 3 feet of a bicycle or scooter on the road \n\nA vehicle nearly hitting a pedestrian in a crosswalk'
     },
     ClosedPath: {
         name: 'Closed Path',
-        uri: require("../assets/images/blocked.png")
+        uri: require("../assets/images/blocked.png"),
+        description: 'Construction/bridge out'
     },
     DocklessVehicleBlockingPath: {
         name: 'Dockless Vehicle Blocking Path',
         uri: require("../assets/images/debris.png"),
-        description:
-            'Things in the road \nRoad Kill \nWhatever'
+        description:'Scooter/rideshare bike in path right of way \n\nScooter/rideshare bike blocking sidewalk'
     },
     Hazard: {
         name: 'Hazard',
-        uri: require("../assets/images/caution.png")
+        uri: require("../assets/images/caution.png"),
+        description: 'Debris in path'
     },
     MalfunctioningSignal: {
         name: 'Malfunctioning Signal',
-        uri: require("../assets/images/eye.png")
-    },
-    PoorLighting: {
-        name: 'Poor Lighting',
-        uri: require("../assets/images/left.png")
+        uri: require("../assets/images/eye.png"),
+        description: 'Crosswalk or traffic signal out \n\nMiscellaneous signal malfunction'
     },
     Pothole: {
         name: 'Pothole',
-        uri: require("../assets/images/cone.png")
+        uri: require("../assets/images/cone.png"),
+        description: 'Missing pavement \n\nLarge cracks in pavement'
     },
-
+    GeneralSafetyConcern: {
+        name: 'General Safety Concern',
+        uri: require("../assets/images/left.png"),
+        description: 'Any concern not listed'
+    }
 }
 export { Icons }
