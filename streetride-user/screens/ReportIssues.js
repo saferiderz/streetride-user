962b7e229eef2e467bb1558206009c49c30c9098
Chris <chris.m.hein@gmail.com>
2019-05-06 14:53:51 -0400

    added message

--- streetride-user/screens/ReportIssues.js
+++ streetride-user/screens/ReportIssues.js
@@ -77,7 +77,7 @@ export default class ReportIssues extends Component {
     if (this.state.latitude === null || this.state.longitude === null) {
       return Alert.alert(
         "Missing Location",
-        "We don't quite have your location yet. Please wait a moment and try to submit again",
+        "We don't quite have your location yet. Please wait a moment and try to submit again.",
         [
           {
             text: "OK",
